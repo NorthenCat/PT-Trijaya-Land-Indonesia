2763e7828d1c5a30f29e19041741907e994ccdd8
NorthenCat <northencat@gmail.com>
2024-12-20 01:50:45 +0700

    update SEO

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,13 @@ let swiper = new Swiper(".swiper", {
     prevEl: ".swiper-prev",
   },
 });
+
+// Menghapus overlay setelah halaman selesai dimuat
+window.addEventListener("load", function () {
+  const loadingOverlay = document.getElementById("loading-overlay");
+  loadingOverlay.style.transition = "opacity 0.5s";
+  loadingOverlay.style.opacity = 0;
+  setTimeout(() => {
+    loadingOverlay.style.display = "none";
+  }, 500); // Sembunyikan overlay sepenuhnya setelah animasi selesai
+});
